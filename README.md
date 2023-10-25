3deda2f451a46f2a04a018aa8c48871793ab7282
WillemW-01 <daeben72@gmail.com>
2023-10-25 14:14:49 +0200

    Added javascript implementation

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ My three main languages are used to implement this, in various styles:
 
 - [x] Pythonic
 - [x] Java OOP
-- [ ] JavaScript HTTP
+- [x] JavaScript
 
 ## Usage
 
@@ -16,15 +16,24 @@ Parameters listed in `< >` are mandatory
 
 Parameters listed in `[ ]` are optional
 
+It is assumed that a `Python 3+`, `Java 8+` and a `Node v16+` environment are
+all installed.
+
 ### 1. Python
 
 ```
-$ python vowel_counter.py <input_word:str> [vowel_list:str]
+$ python vowel_counter.py <input_word:str> [vowel_space:str]
 ```
 
 ### 2. Java
 
 ```
 $ javac VowelCounter.java
-$ java VowelCounter <String inputWord> [String vowelList]
+$ java VowelCounter <String inputWord> [String vowelSpace]
+```
+
+### 3. JavaScript
+
+```
+$ node vowelCounter.js <inputWord:String> [vowelSpace:String]
 ```
---
+++ vowelCounter.js
@@ -0,0 +1,29 @@
+/**
+ * This program counts the number of vowels contained in the input string.
+ *
+ * It uses a regex approach by counting all the matches for each letter
+ * in the vowel space.
+ *
+ * Assumptions:
+ * ------------
+ *  - all letters in input word are Unicode
+ *  - no letters are punctuation marks (especially not "|")
+ */
+
+// first, get the input word and vowel space from the terminal
+let inputWord = process.argv[2];
+let vowels = "aeiou".split("");
+if (process.argv.length == 4) {
+  vowels = process.argv[3].split("");
+}
+
+// then we create a regex string that will match any of the vowels
+// e.g. "(a|e|i|o|u)"
+let regex_string = vowels.join("|");
+
+// then we match all occurrences and count the length of the matches
+let matches = [...inputWord.matchAll(`(${regex_string})`)];
+let num_vowels = matches.length;
+
+console.log(`Vowel space: ${vowels}`);
+console.log(`Number of vowels in '${inputWord}': ${num_vowels}`);
